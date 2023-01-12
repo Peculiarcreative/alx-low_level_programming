a2570e9f49a9a6aa5598336ca5dae203fa900a51
Peculiarcreative <peculiargrafiz30@gmail.com>
2023-01-12 15:36:21 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,15 +5,15 @@
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
-{  
-  int counter = 0; 
+{
+  int counter = 0;
   if (h == NULL)
     return (counter);
   while (h->prev != NULL)
     h = h->prev;
   while (h != NULL)
     {
-      printf("%d\n", h->n); 
+      printf("%d\n", h->n);
       h = h->next;
       counter++;
     }
